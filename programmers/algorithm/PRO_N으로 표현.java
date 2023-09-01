b1c2b4c796a39d12904608099e2faffe3a81610e
L-Woo <91431779+L-Woo@users.noreply.github.com>
2023-09-02 04:16:48 +0900

    Update PRO_N으로 표현.java

--- programmers/algorithm/PRO_N으로 표현.java
+++ programmers/algorithm/PRO_N으로 표현.java
@@ -1,4 +1,5 @@
 // 최솟값이 8보다 크면 -1을 리턴
+// 
 
 import java.util.*; 
 
@@ -8,25 +9,27 @@ class Solution {
         
         Set<Integer>[] set = new HashSet[9];
         int num = 0;
+        // 사용횟수로 모두 사용해서 만들 수 있는 수 ex) 55, 555
         for(int i = 1; i < 9; i++) {
             num = (num * 10) + N;
             set[i] = new HashSet<>();
             set[i].add(num);
         }
-        
         for(int i = 1; i < 9; i++) {
             for(int j = 1; j < i; j++) {
                 for(Integer a : set[j]) {
-                    for(Integer b : set[i - j]) {    
+                    for(Integer b : set[i - j]) {
                         set[i].add(a + b);
                         set[i].add(a - b);
                         set[i].add(a * b);
+                        // 0으로 나눌 수 없기 때문에 0이 아닌 수로 나눌 수 있도록 해야한다.
                         if(b != 0) {
                             set[i].add(a / b);
                         }  
                         if(a != 0) {
                             set[i].add(b / a);
-                        }    
+                        }
+                            
                     }
                 }    
             }
@@ -34,7 +37,6 @@ class Solution {
                     return i;
             }
         }
-    
         return answer;
     }
 }

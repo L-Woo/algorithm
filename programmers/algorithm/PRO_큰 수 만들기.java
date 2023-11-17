c3fb04cbb8612e0817b88533b56c81d85a156f4c
L-Woo <91431779+L-Woo@users.noreply.github.com>
2023-11-17 15:48:13 +0900

    Update PRO_큰 수 만들기.java

--- programmers/algorithm/PRO_큰 수 만들기.java
+++ programmers/algorithm/PRO_큰 수 만들기.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        String answer = "";
         StringBuilder sb = new StringBuilder();
         
         char[] arr = number.toCharArray();

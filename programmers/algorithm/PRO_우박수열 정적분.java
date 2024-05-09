71bd6bf99f5e4e68943b9c0e1fc4f9d4aaaf1bd3
L-Woo <91431779+L-Woo@users.noreply.github.com>
2024-05-09 21:09:55 +0900

    Create PRO_우박수열 정적분.java

---
+++ programmers/algorithm/PRO_우박수열 정적분.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public double[] solution(int k, int[][] ranges) {
+        double[] answer = new double[ranges.length];
+        
+        List<Integer> list = new ArrayList<>();
+        while(k > 1) {
+            list.add(k);
+            if(k % 2 == 0) k = k / 2;
+            else k = k * 3 + 1;
+        }
+        list.add(k);
+        
+        for(int i = 0; i < ranges.length; i++) {
+
+            int start = ranges[i][0];
+            int end = list.size() - 1 + ranges[i][1];
+
+            if(start > end) {
+                answer[i] = -1;
+                continue;
+            }
+            
+            double sum = 0;
+            for(int j = start; j < end; j++) {
+                double now = list.get(j);
+                double target = list.get(j + 1);
+                sum += (now + target) / 2;
+            }
+            answer[i] = sum;
+        }
+        
+        return answer;
+    }
+}

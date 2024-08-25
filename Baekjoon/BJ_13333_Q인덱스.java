147575efa82cf3f4311d9990b170810f5dcd137e
L-Woo <91431779+L-Woo@users.noreply.github.com>
2024-08-25 18:01:17 +0900

    Create BJ_13333_Q인덱스.java

---
+++ Baekjoon/BJ_13333_Q인덱스.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_13333_Q인덱스 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        int qIdx = 0;
+        for (int k = 1; k <= N; k++) {
+            if (arr[N - k] >= k) {
+                qIdx = k;
+            }
+        }
+
+        System.out.println(qIdx);
+    }
+
+}

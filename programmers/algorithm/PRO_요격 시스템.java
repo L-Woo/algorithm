4cc5febc57c96a8b47b0162bf1a4bf12509ce244
L-Woo <91431779+L-Woo@users.noreply.github.com>
2024-09-06 23:13:09 +0900

    Create PRO_요격 시스템.java

---
+++ programmers/algorithm/PRO_요격 시스템.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        
+        Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
+        
+        int missile = 0;
+        for(int i = 0;i < targets.length; i++){
+            if(missile <= targets[i][0]){
+                missile = targets[i][1];
+                answer++;
+            }
+        }
+
+        
+        return answer;
+    }
+}

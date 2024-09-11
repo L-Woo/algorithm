7cfad215732d2c01637d9d6a8057b2f2f117ce0a
L-Woo <91431779+L-Woo@users.noreply.github.com>
2024-09-11 21:19:07 +0900

    Create BJ_1072_게임.java

---
+++ Baekjoon/BJ_1072_게임.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_1072_게임 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int z = getPercent(x, y);
+
+        int ans = -1;
+        int left = 0;
+        int right = 1000000000;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (getPercent(x + mid, y + mid) != z) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static int getPercent(int x, int y) {
+        return (int) ((long) y * 100 / x);
+    }
+
+}

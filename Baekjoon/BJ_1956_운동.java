d0c40c7f9e21431a21ceba986101f1dc7aa09891
L-Woo <91431779+L-Woo@users.noreply.github.com>
2024-11-20 13:24:28 +0900

    Create BJ_1956_운동.java

---
+++ Baekjoon/BJ_1956_운동.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_1956_운동 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int[][] map = new int[V + 1][V + 1];
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= V; i++) Arrays.fill(map[i], 100000000);
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            map[start][end] = cost;
+        }
+
+        for (int k = 1; k <= V; k++) {
+            for (int i = 1; i <= V; i++) {
+                for (int j = 1; j <= V; j++) {
+                    map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= V; i++) ans = Math.min(ans, map[i][i]);
+        System.out.println(ans >= 100000000 ? -1 : ans);
+
+    }
+}

eeeee37e12a28676d6e19b05bc5019cac427bec2
L-Woo <91431779+L-Woo@users.noreply.github.com>
2026-01-25 16:29:29 +0900

    Create BJ_2410_2의멱수의합.java

---
+++ Baekjoon/BJ_2410_2의멱수의합.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_2410_2의멱수의합 {
+
+    private static final int MOD = 1_000_000_000;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp  = new long[N + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= N; i++) {
+            dp[i] = (dp[i - 2] + dp[i / 2]) % MOD;
+        }
+        System.out.println(dp[N]);
+    }
+
+}
